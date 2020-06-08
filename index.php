545f96f9b6c0d95bc4d27b23c527996c86bdeb03
lucero <pucheros.samy@gmail.com>
2020-06-07 19:21:40 -0500

    nuevo php

---
+++ index.php
@@ -0,0 +1,276 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  
+
+    <style type="text/css">
+
+
+      .jumbotron{
+      margin-top: 20px;
+      background-image:  url(IMAGENES/BANNER1.jpg);
+      background-size: cover;
+      background-repeat: no-repeat;
+      color: transparent;
+      }
+    </style>
+
+    <title>MUNDO VINTAGE</title>
+
+
+
+    <link rel="stylesheet" type="text/css" href="css/estilo.css">
+
+<link href="https://fonts.googleapis.com/css?family=Lobster|Lobster+Two:700i&display=swap" rel="stylesheet">
+<style>
+  h1{
+  font-family: 'Lobster Two', cursive;
+  }
+</style>
+
+
+<style>
+  h2 {
+  font-family: 'Lobster Two', cursive;
+  }
+</style>
+
+
+<style>
+  h5 {
+  font-family: 'Lobster Two', cursive;
+  }
+</style>
+
+
+
+
+
+  </head>
+  <body >
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item ">
+        <a class="nav-link " href="index.html"><img src="https://img.icons8.com/small/16/000000/home.png"/>INICIO <span class="sr-only">(current)</span></a>
+      </li>
+      
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img src="https://img.icons8.com/ios/16/000000/pictures-folder.png"/>
+          GALERIA
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="GAMER RETRO.html"><img src="https://img.icons8.com/ios/16/000000/controller.png"/>GAMER RETRO</a>
+          <a class="dropdown-item" href="CAMARA.html"><img src="https://img.icons8.com/ios/16/000000/old-time-camera.png"/>CAMARA</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="DECORACION.html"><img src="https://img.icons8.com/ios/16/000000/sofa.png"/>DECORACION</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link " href="FUCION RETRO.html" tabindex="-1" aria-disabled="true"><img src="https://img.icons8.com/ios/16/000000/3d-glasses.png"/>FUCION RETRO</a>
+      </li>
+          <li class="nav-item">
+        <a class="nav-link" href="MI CUENTA.html"><img src="https://img.icons8.com/ios/16/000000/add-user-male.png"/> MI CUENTA </a>
+      </li>
+    </ul>
+
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="BUSCAR" aria-label="search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">BUSCAR</button>
+    </form>
+  </div>
+</nav>
+
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-1 text-right text-info"> Mundo vintage</h1>
+  </div>
+</div>
+<div class= mt-5>
+    <p><h2><center>Coleccione momentos no cosas</h2></center></p>
+
+<br>
+   <div class="card-columns"  >
+  <div class="card"style="background:#EFF4F7 ">
+    <img src="IMAGENES/PRUEBA 1.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Mundo</h5>
+      <p class="card-text">Mira la vida no solo en tus ojos.</p>
+    </div>
+  </div>
+  <div class="card p-3" style="background:#EFF4F7 ">
+    <blockquote class="blockquote mb-0 card-body">
+      <p>Las revistas de diseño de interiores mostraban a menudo el estilo retro como una decoración interior de una mezcla de estilos y objetos del pasado, de segunda mano y nuevos. Por ejemplo, papeles pintados de la década de los 70, combinados con muebles de segunda mano de las décadas de los 50 y 60. El valor de lo antiguo se incrementó porque el objeto pasó a ser considerado anticuado pero para todos los días. En este caso, ‘retro’ indica un valor, pero también parcial, porque los fabricantes actuales producen nuevos objetos con el estilo antiguo.</p>
+      <footer class="blockquote-footer">
+        <small class="text-muted">
+         RETRO  <cite title="Source Title"> muchas personas usan el término para categorizar estilos que se han creado en el pasado. El estilo retro refiere a cosas nuevas que muestran características del pasado. </cite>
+        </small>
+      </footer>
+    </blockquote>
+  </div>
+  <div class="card"style="background:#EFF4F7 ">
+    <img src="IMAGENES/PRUEBA3.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">La fotografia</h5>
+      <p class="card-text">es un secreto de un secreto cuanto mas te dice, menos sabes.</p>
+      <p class="card-text"><small class="text-muted">dibujar con luz</small></p>
+    </div>
+  </div>
+  <div class="card p-3 mb-2 bg-info text-white  text-center p-3">
+    <blockquote class="blockquote mb-0">
+      <p>MUNDO RETRO Y VINTAGE.</p>
+      <footer class="blockquote-footer text-white">
+        <small>
+          mezclando <cite title="Source Title">DOS MUNDOS </cite>
+        </small>
+      </footer>
+    </blockquote>
+  </div>
+  <div class="card text-center"style="background:#EFF4F7 ">
+    <div class="card-body">
+      <h5 class="card-title">Retro</h5>
+      <p class="card-text"> se trata de objetos que evocan al pasado, pero que no deben necesariamente pertenecer a él.</p>
+      <p class="card-text"><small class="text-muted">simplemente emplean la estética de otra época </small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="IMAGENES/RAME.jpg" class="card-img-top" alt="...">
+  </div>
+  <div class="card p-3 text-right"style="background:#EFF4F7 ">
+    <blockquote class="blockquote mb-0">
+      <p>RAME  Algo que es caotico y hermoso al mismo tiempo  .</p>
+      <footer class="blockquote-footer">
+        <small class="text-muted">
+        LOVE FOR  <cite title="Source Title">VINTAGE</cite>
+        </small>
+      </footer>
+    </blockquote>
+  </div>
+  <div class="card">
+    <div class="card-body" style="background:#EFF4F7 ">
+      <h5 class="card-title">Vintage </h5>
+      <p class="card-text">vintage hace referencia directa al pasadoa través de prendas u objetos que fueron diseñados y fabricados en la época a la que pertenece su estilo.</p>
+      <p class="card-text"><small class="text-muted">siempre que tenga algo que aportar estéticamente, y no sea simplemente viejo.</small></p>
+    </div>
+  </div>
+</div>
+
+
+
+<br>
+<div class="card text-center">
+  <div class="card-header" ">
+     RECOMENDADOS
+  </div>
+  <div class="card-body" style="background:#F0FFF0 ">
+    <h5 class="card-title"> Vintage</h5>
+    <p class="card-text"> dale click al boton.</p>
+    <a href="https://www.youtube.com/watch?v=6wHjTVGGcMM" class="btn btn-primary">ver video</a>
+  </div>
+  <div class="card-footer text-muted" >
+    esperamos que te guste 
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="js/mini.js"></script>
+  </body>
+</html>
